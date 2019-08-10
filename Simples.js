e8ee7e3beb9ebf98773482e92202fc8f2fe2cfc5
AurioPinto <auriopinto@outlook.com>
2019-08-11 05:18:19 +0800

    Upload

---
+++ Simples.js
@@ -0,0 +1,12 @@
+// Event Emitters
+
+const EventEmitter = require('events')
+
+class Job extends EventEmitter {}
+job = new Job()
+
+job.on('done', function(timeDone){
+    console.log('Job was Pronouced done at', timeDone)
+})
+job.emit('done', new Date())
+job.removeAllListeners()
---
+++ Weekly.js
@@ -0,0 +1,9 @@
+var Job = require('./Jobs.js')
+var job = new Job()
+
+job.on('done', function(details){
+  console.log('Weekly email job was completed at',
+    details.completedOn)
+})
+
+job.emit('start')
